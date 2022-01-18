ca3b6bf7fa63687b99bbeab79874d742ceb6a0d1
Fares <fares.bourih@gmail.com>
2022-01-18 15:54:58 +0100

    feature add firebase and navigation to project

---
+++ src/screens/private/Home.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import { Text } from "native-base";
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Hello im the Home screen 💙</Text>
+        </View>
+    );
+}
+
+export default Login;
---
+++ src/screens/public/Login.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import { Text } from "native-base";
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Login screen 😁</Text>
+        </View>
+    );
+}
+
+export default Login;
