e6468e0aaebd70db19a8121dd21717ab08ad016d
yacine-21 <yacinelyoubi37@gmail.com>
2022-01-25 15:40:51 +0100

    add activity indicator

--- src/screens/Loading.jsx
+++ src/screens/Loading.jsx
@@ -1,10 +1,11 @@
 import { Button, Center, Text } from "native-base";
+import {ActivityIndicator} from "react-native"
 import * as React from 'react';
 
 const Loading = () => {
     return (
         <Center h="full">
-            <Text>Loading...</Text>
+            <ActivityIndicator size="large" color="#0000ff" />
         </Center>
     );
 }
