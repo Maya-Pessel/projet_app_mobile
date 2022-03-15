fb6df5155834576326efc639e10102e165e20467
Fares <fares.bourih@gmail.com>
2022-03-15 14:12:22 +0100

    fix messageDetails screen position

--- src/RootNavigator.jsx
+++ src/RootNavigator.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useTranslation } from 'react-i18next';
 import firebase from "firebase";
-
+import { Center, Spinner, Text, View } from "native-base";
 // import Loading from "./screens/Loading";
 import AppPrivate from './screens/private';
 import ROUTES from "./routes";
@@ -14,14 +14,16 @@ const Stack = createNativeStackNavigator();
 const RootNavigator = () => {
   const { t } = useTranslation("Private");
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     // onAuthStateChanged returns an unsubscriber
     const unsubscribeAuth = firebase.auth().onAuthStateChanged(async authenticatedUser => {
+      setIsLoading(true);
       try {
         await (authenticatedUser ? setUser(authenticatedUser) : setUser(null));
-        // setIsLoading(false);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -34,42 +36,53 @@ const RootNavigator = () => {
 
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="home">
-        {user !== null 
-        ? (
-            <>
-              <Stack.Screen name="AppPrivate" component={AppPrivate} options={{ header: () => null }} />
-              {ROUTES.map(route => {
-                if (route.layout === "/private" && !route.menuTab && route.screen) {
-                  return <Stack.Screen
-                    key={route.name}
-                    name={route.name}
-                    component={route.screen}
-                    options={{
-                      title: t(`${route.name}.head_title`),
-                    }}
-                  />
-                }
-                return null;
-              })}
-            </>
-        ) : (
-          <Stack.Group  screenOptions={{ headerShown: false }}>
-              {ROUTES.map(route => {
-                if (route.layout === "/public" && route.screen) {
-                  return <Stack.Screen
-                    key={route.name}
-                    name={route.layout + "/" + route.name}
-                    component={route.screen}
-                  />
-                }
-                return null;
-              })}
-          </Stack.Group>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View flex={1}>
+
+      {isLoading && (
+        <Center position="absolute" top={0} bottom={0} left={0} right={0} zIndex={2}>
+          <Spinner size={48} color="primary" />
+        </Center>
+      )}
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="home">
+          {user !== null 
+          ? (
+              <>
+                <Stack.Screen name="AppPrivate" component={AppPrivate} options={{ header: () => null }} />
+                {ROUTES.map(route => {
+                  if (route.layout === "/private" && !route.menuTab && route.screen) {
+                    return <Stack.Screen
+                      key={route.name}
+                      name={route.name}
+                      component={route.screen}
+                      options={{
+                        title: t(`${route.name}.head_title`),
+                      }}
+                    />
+                  }
+                  return null;
+                })}
+              </>
+          ) : (
+            <Stack.Group  screenOptions={{ headerShown: false }}>
+                {ROUTES.map(route => {
+                  if (route.layout === "/public" && route.screen) {
+                    return <Stack.Screen
+                      key={route.name}
+                      name={route.layout + "/" + route.name}
+                      component={route.screen}
+                    />
+                  }
+                  return null;
+                })}
+            </Stack.Group>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </View>
+
   );
 };
 
--- src/screens/private/MessageDetails.jsx
+++ src/screens/private/MessageDetails.jsx
@@ -1,13 +1,19 @@
-import {Text, Center, Box, TextArea, HStack} from "native-base";
+import {Text, Center, Box, TextArea, HStack, View, ScrollView} from "native-base";
 import Button from "../../components/Button";
 
 const MessageDetails = () => {
     return (
 
-      <HStack bg={"black"} p={3}>
-        <TextArea bg={"white"} borderRadius={0} borderWidth={0} w="70%" maxW="300" h={10} />
-        <Button borderRadius={0} bg={"white"}>Envoyer</Button>
-      </HStack>
+      <View flex={1}>
+        <ScrollView flex={1}>
+          <Text>Hello</Text>
+        </ScrollView>
+
+        <HStack bg={"black"} p={3}>
+          <TextArea bg={"white"} borderRadius={0} borderWidth={0} w="70%" maxW="300" h={10} />
+          <Button borderRadius={0} bg={"white"}>Envoyer</Button>
+        </HStack>
+      </View>
     );
 }
 
